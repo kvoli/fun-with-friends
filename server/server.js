d895062d56191769e02290c4ac0deb7525e6b655
Ben <dezyh@outlook.com>
2019-08-04 19:17:19 +1000

    changed build to production mode for the build pipeline

--- server/server.js
+++ server/server.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+const client_directory = path.join(__dirname, '../client/build/');
 
-app.listen(port, () => {
-  console.log(`Server Started on Port ${port}`);
-})
+app.use(express.static(client_directory));
 
 app.get('/', (req, res) => {
   res.send('Express Server <br> <a href="http://localhost:3000">React Client</a>');
 });
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(client_directory, 'index.html'));
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => {
+  console.log(`Server Started on Port ${port}`);
+})
