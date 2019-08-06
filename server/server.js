ee31f8f6fc37bec18ad1230b9244b96d7806f316
Ben <dezyh@outlook.com>
2019-08-06 20:01:12 +1000

    Add different startup options depending on the environment variable NODE_ENV
    Server now starts in production mode when NODE_ENV=production and otherwise starts in development mode. This can be set via the command 'export NODE_ENV = production'.

--- server/server.js
+++ server/server.js
@@ -3,16 +3,18 @@ const path = require('path');
 
 const app = express();
 
-const client_directory = path.join(__dirname, '../client/build/');
-
-app.use(express.static(client_directory));
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(client_directory, 'index.html'));
-});
+if (process.env.NODE_ENV === 'production') {
+  const client_directory = path.join(__dirname, '../client/build/');
+  app.use(express.static(client_directory));
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(client_directory, 'index.html'));
+  });
+  console.log('Running in production mode')
+} else {
+  console.log('Running in development mode')
+}
 
 const port = process.env.PORT || 8080;
-
 app.listen(port, () => {
   console.log(`Server Started on Port ${port}`);
 })
