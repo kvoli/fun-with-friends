d72259c656466ebab28f7866076ce4fe23884fea
Ben Mitchell <dezyh@outlook.com>
2019-08-20 17:32:57 +1000

    Fix formatting
    Removed some blank lines, added some semicolons to the import statements and changed the tab indent to 2 spaces instead of 4.

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -2,38 +2,35 @@ import React from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import TypoGraphy from '@material-ui/core/Typography'
-import { Home, Book, AccountBox } from '@material-ui/icons'
+import TypoGraphy from '@material-ui/core/Typography';
+import { Home, Book, AccountBox } from '@material-ui/icons';
 
 const NavBar = () => {
+  return (
+    <List component="nav">
+      <ListItem component="div" >
 
-    return (
-        <List component="nav">
-            <ListItem component="div" >
+        <ListItemText inset>
+          <TypoGraphy color="inherit" variant="title">
+            Home  <Home />
+          </TypoGraphy>
+        </ListItemText>
 
-                <ListItemText inset>
-                    <TypoGraphy color="inherit" variant="title">
-                        Home  <Home />
-                    </TypoGraphy>
-                </ListItemText>
+        <ListItemText inset>
+          <TypoGraphy color="inherit" variant="title">
+            Create <Book />
+          </TypoGraphy>
+        </ListItemText>
 
-
-                <ListItemText inset>
-                    <TypoGraphy color="inherit" variant="title">
-                        Create <Book />
-                    </TypoGraphy>
-                </ListItemText>
-
-                <ListItemText inset>
-                    <TypoGraphy color="inherit" variant="title">
-                        Account <AccountBox />
-                    </TypoGraphy>
-                </ListItemText>
-            </ListItem >
-
-        </List>
-    )
+        <ListItemText inset>
+          <TypoGraphy color="inherit" variant="title">
+            Account <AccountBox />
+          </TypoGraphy>
+        </ListItemText>
+    
+      </ListItem >
+    </List>
+  )
 }
-
 
 export default NavBar;
