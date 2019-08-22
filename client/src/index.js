259d455389c26be00de738320fb3b07ce75ad2a7
Ben <dezyh@outlook.com>
2019-08-23 01:13:15 +1000

    Add react routing, signup page and reordered routing on the server
    Reordered routing on the server so that API requests should get picked up by the server and not the client

--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,13 @@ import { render } from "react-dom";
 import { Provider } from "react-redux";
 import store from "./js/store/index";
 import App from "./js/components/App.jsx";
-
+import { BrowserRouter } from 'react-router-dom';
 
 render (
 	<Provider store={store}>
-	<App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
 	</Provider>,
 	document.getElementById("root")
 );
