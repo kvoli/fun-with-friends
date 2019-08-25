49db6157acd41b282b3059aef5af14a138545f34
Ben <dezyh@outlook.com>
2019-08-25 23:40:53 +1000

    Fix a depreciated function call

--- server/models/db.js
+++ server/models/db.js
@@ -3,7 +3,8 @@ var mongoose = require('mongoose');
 // Define the connection url and options
 const DbConnection = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_TABLE}?${process.env.DB_OPTIONS}`;
 const Options = {
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useCreateIndex: true,
 }
 
 // Connect to the database with the connection url and options
