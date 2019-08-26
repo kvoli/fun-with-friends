eac9c60845225b1964e872c944602613d380da53
Ben <dezyh@outlook.com>
2019-08-27 01:58:30 +1000

    Fix a warning

--- client/src/js/components/App.jsx
+++ client/src/js/components/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { PersistGate } from 'redux-persist/integration/react';
 
 import Main from './Main';
 import NavBar from './NavBar';
