dc0ac31cfa68380cd46d3767bbf80fe1eaf7887a
alanlewis764 <49027863+alanlewis764@users.noreply.github.com>
2019-08-27 21:09:40 +1000

    Changes made to actions, components-navProfileMenu, components/SignUp… (#54)
    * Changes made to actions, components-navProfileMenu, components/SignUp, constants and reducers to allow for sign up and logout
    
    * update auth to fix logout logic

--- client/src/js/components/NavProfileMenu.jsx
+++ client/src/js/components/NavProfileMenu.jsx
@@ -2,19 +2,24 @@ import React from 'react';
 import MenuItem from '@material-ui/core/MenuItem';
 import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { logout } from '../actions/auth';
 
 
 export const NavProfileMenu = ({handleClose}) => {
   const store = useSelector(store => store);
+  const dispatch = useDispatch();
   if (store.auth.success) {
     // User is authenticated
     return (
       <div>
         <MenuItem onClick={handleClose}>{'Hi ' + store.auth.user.firstname + ' ' + store.auth.user.lastname}</MenuItem>
-        <MenuItem onClick={handleClose}><NavLink to='/logout'>Logout</NavLink></MenuItem>
+        <MenuItem onClick={() => dispatch(logout())}>
+          <NavLink to='/login'>Logout</NavLink></MenuItem>
       </div>
     );
-  } else {
+  } 
+  else {
     // User is not authenticated
     return (
       <div>
