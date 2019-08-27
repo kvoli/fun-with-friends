1b69e6cd214c0fcbbfad85d88fb3c32d350c83dd
alanlewis764 <lewisa1@student.unimelb.edu.au>
2019-08-27 20:56:29 +1000

    Changes made to actions, components-navProfileMenu, components/SignUp, constants and reducers to allow for sign up and logout

--- client/src/js/components/SignUp.jsx
+++ client/src/js/components/SignUp.jsx
@@ -11,6 +11,9 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { signup } from '../actions/auth';
 
 const useStyles = makeStyles(theme => ({
   '@global': {
@@ -38,6 +41,13 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const SignUp = () => {
+  const dispatch = useDispatch();
+  const [firstname, setFirstname] = useState('');
+  const [lastname, setLastname] = useState('');
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  
   const classes = useStyles();
   return (
     <Container component="main" maxWidth="xs">
@@ -53,6 +63,7 @@ const SignUp = () => {
           <Grid container spacing={2}>
             <Grid item xs={12} sm={6}>
               <TextField
+                onChange = {(e) => {setFirstname(e.target.value)}}
                 autoComplete="fname"
                 name="firstName"
                 variant="outlined"
@@ -65,6 +76,7 @@ const SignUp = () => {
             </Grid>
             <Grid item xs={12} sm={6}>
               <TextField
+                onChange = {(e) => {setLastname(e.target.value)}}
                 variant="outlined"
                 required
                 fullWidth
@@ -76,6 +88,7 @@ const SignUp = () => {
             </Grid>
             <Grid item xs={12}>
               <TextField
+                onChange = {(e) => {setEmail(e.target.value)}}
                 variant="outlined"
                 required
                 fullWidth
@@ -85,8 +98,21 @@ const SignUp = () => {
                 autoComplete="email"
               />
             </Grid>
+            <Grid item xs={12}>  
+              <TextField 
+                onChange = {(e) => {setUsername(e.target.value)}}
+                variant="outlined"
+                required
+                fullWidth
+                id="username"
+                label="username"
+                name="username"
+                autoComplete="username"
+              />
+            </Grid>
             <Grid item xs={12}>
               <TextField
+                onChange = {(e) => {setPassword(e.target.value)}}
                 variant="outlined"
                 required
                 fullWidth
@@ -105,7 +131,7 @@ const SignUp = () => {
             </Grid>
           </Grid>
           <Button
-            type="submit"
+            onClick = {(e) => {dispatch(signup(firstname, lastname, email, username, password))}}
             fullWidth
             variant="contained"
             color="primary"
