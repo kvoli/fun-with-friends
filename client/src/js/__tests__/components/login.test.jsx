fb5e733b6e39e861711e241155191b70e5cf4a8d
voli <amcclernon@student.unimelb.edu.au>
2019-09-06 00:12:11 +1000

    remove redundant imports into file testing >>> to pass linting

--- client/src/js/__tests__/components/login.test.jsx
+++ client/src/js/__tests__/components/login.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Provider} from 'react-redux'
-import { mount, shallow } from 'enzyme'
+import { shallow } from 'enzyme'
 import configureMockStore from 'redux-mock-store'
 import thunk from 'redux-thunk';
 import Login from '../../components/Login';
