fe144acf63f6624e753f4144f6da4f4ca265b886
alanlewis764 <lewisa1@student.unimelb.edu.au>
2019-09-06 01:08:57 +1000

    update for database testing with resolving merge conflicts

--- server/test/artifact_test.js
+++ server/test/artifact_test.js
@@ -1,19 +1,32 @@
 const Artifact = require('../models/artifact');
-const expect = require('chai').expect;
+const mocha = require('mocha');
+const chain = require('chai');
+var expect = require('chai').expect;
 
-describe("Testing to see if required artifact properties are mandatory", function() {
-  var artifact = new Artifact()
-  it("Artifact requires an _id", function(){
-    expect(artifact).to.have.property("_id");
+describe('Testing to see if required artifact properties are mandatory', function() {
+  var artifact = new Artifact(
+    {
+      _id: '1234',
+      title: 'test', 
+      desc: 'testing testing testing',
+      uploader: 'Alan Lewis'
+    }
+  )
+  it('Artifact requires an _id', function(){
+    expect(artifact).to.have.property('_id');
   })
-  it("Artifact requires a title", function(){
-    expect(artifact).to.have.property("title");
+
+  it('Artifact requires a title', function(){
+    expect(artifact).to.have.property('title');
   })
-  it("Artifact requires a description", function(){
-    expect(artifact).to.have.property("desc");
+
+  it('Artifact requires a description', function(){
+    expect(artifact).to.have.property('desc');
   })
-  it("Object requires a uploaded date", function(){
-    expect(artifact).to.have.property("uploaded");
+
+  it('Object requires a uploaded date', function(){
+    expect(artifact).to.have.property('uploaded');
   })
+
 })
 
--- server/test/user_test.js
+++ server/test/user_test.js
@@ -1,31 +1,33 @@
 const User = require('../models/user');
 const assert = require('chai').assert;
 const expect = require('chai').expect;
+const mocha = require('mocha');
+const chain = require('chai');
 
-describe("Testing to see if required user properties are mandatory", function() {
+describe('Testing to see if required user properties are mandatory', function() {
   var user = new User({
-    firstname: "",
-    lastname: "Smith",
-    email: "johnsmith@gmail.com",
-    username: "johnsmith",
-    password: "password"
+    firstname: '',
+    lastname: 'Smith',
+    email: 'johnsmith@gmail.com',
+    username: 'johnsmith',
+    password: 'password'
   })
-  it("User requires an firstname", function(){
-    expect(user).property("firstname");
+  it('User requires an firstname', function(){
+    expect(user).property('firstname');
   })
-  it("User requires a lastname", function(){
-    expect(user).to.have.property("lastname");
+  it('User requires a lastname', function(){
+    expect(user).to.have.property('lastname');
   })
-  it("User requires an email", function(){
-    expect(user).to.have.property("email");
+  it('User requires an email', function(){
+    expect(user).to.have.property('email');
   })
-  it("User requires a username", function(){
-    expect(user).to.have.property("username");
+  it('User requires a username', function(){
+    expect(user).to.have.property('username');
   })
-  it("User requires a password", function(){
-    expect(user).to.have.property("password");
+  it('User requires a password', function(){
+    expect(user).to.have.property('password');
   })
-  it("User password should be 6 or more characters", function(){
-    expect(user).to.have.property("password").with.length.greaterThan(6);
+  it('User password should be 6 or more characters', function(){
+    expect(user).to.have.property('password').with.length.greaterThan(6);
   })
 })
