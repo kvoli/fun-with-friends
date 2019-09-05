7b440f8d443338727b43db78d01cb2b00703b381
alanlewis764 <lewisa1@student.unimelb.edu.au>
2019-09-06 09:52:23 +1000

    changes for codacy

--- server/test/user_test.js
+++ server/test/user_test.js
@@ -1,5 +1,5 @@
 const User = require('../models/user');
-const expect = require('chai').expect;
+const { expect } = require('chai');
 
 describe('Testing to see if required user properties are mandatory', function() {
   var user = new User({
@@ -8,23 +8,23 @@ describe('Testing to see if required user properties are mandatory', function() 
     email: 'johnsmith@gmail.com',
     username: 'johnsmith',
     password: 'password'
-  })
-  it('User requires an firstname', function(){
+  });
+  it('User requires an firstname', () => {
     expect(user).property('firstname');
-  })
-  it('User requires a lastname', function(){
+  });
+  it('User requires a lastname', () => {
     expect(user).to.have.property('lastname');
-  })
-  it('User requires an email', function(){
+  });
+  it('User requires an email', () => {
     expect(user).to.have.property('email');
-  })
-  it('User requires a username', function(){
+  });
+  it('User requires a username', () => {
     expect(user).to.have.property('username');
-  })
-  it('User requires a password', function(){
+  });
+  it('User requires a password', () => {
     expect(user).to.have.property('password');
-  })
-  it('User password should be 6 or more characters', function(){
+  });
+  it('User password should be 6 or more characters', () => {
     expect(user).to.have.property('password').with.length.greaterThan(6);
-  })
+  });
 })
