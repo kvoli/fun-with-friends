6e876d8a7f64bba59163e9aadd0b47c8701d7834
alanlewis764 <lewisa1@student.unimelb.edu.au>
2019-09-06 01:03:59 +1000

    Revert "changed double quotes to single quotes to pass codacy"
    This reverts commit 6d8dab17b25df3179ad42800a68988f888f4f6e9.

--- server/test/user_test.js
+++ server/test/user_test.js
@@ -1,32 +1,31 @@
 const User = require('../models/user');
 const assert = require('chai').assert;
 const expect = require('chai').expect;
-const should = require('chai').should;
 
-describe('Testing to see if required user properties are mandatory', function() {
+describe("Testing to see if required user properties are mandatory", function() {
   var user = new User({
-    firstname: 'John',
-    lastname: 'Smith',
-    email: 'johnsmith@gmail.com',
-    username: 'johnsmith',
-    password: 'password'
+    firstname: "",
+    lastname: "Smith",
+    email: "johnsmith@gmail.com",
+    username: "johnsmith",
+    password: "password"
   })
-  it('User requires an firstname', function(){
-    expect(user).to.have.property('firstname');
+  it("User requires an firstname", function(){
+    expect(user).property("firstname");
   })
-  it('User requires a lastname', function(){
-    expect(user).to.have.property('lastname');
+  it("User requires a lastname", function(){
+    expect(user).to.have.property("lastname");
   })
-  it('User requires an email', function(){
-    expect(user).to.have.property('email');
+  it("User requires an email", function(){
+    expect(user).to.have.property("email");
   })
-  it('User requires a username', function(){
-    expect(user).to.have.property('username');
+  it("User requires a username", function(){
+    expect(user).to.have.property("username");
   })
-  it('User requires a password', function(){
-    expect(user).to.have.property('password');
+  it("User requires a password", function(){
+    expect(user).to.have.property("password");
   })
-  it('User password should be 6 or more characters', function(){
-    expect(user).to.have.property('password').with.length.greaterThan(6);
+  it("User password should be 6 or more characters", function(){
+    expect(user).to.have.property("password").with.length.greaterThan(6);
   })
 })
