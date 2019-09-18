b32aa36d8b9101bb04f9f5c147823678bf7eb86f
voli <amcclernon@student.unimelb.edu.au>
2019-09-18 12:03:25 +1000

    resolve merge conflicts 2

--- client/src/js/actions/circles.js
+++ client/src/js/actions/circles.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import { ADD_CIRCLE, ADD_CIRCLE_USER, REMOVE_CIRCLE, REMOVE_CIRCLE_USER } from "../constants/action-types.js";
-=======
 import { ADD_CIRCLE, ADD_CIRCLE_USER, REMOVE_CIRCLE, REMOVE_CIRCLE_USER, OPEN_CIRCLE_FORM } from "../constants/action-types.js";
->>>>>>> kvoli-grouppage
 
 export const addCircle = (payload) => ({
 	type: ADD_CIRCLE,
@@ -23,11 +19,8 @@ export const removeCircleUser = (payload) => ({
 	type: REMOVE_CIRCLE_USER,
 	payload: payload
 });
-<<<<<<< HEAD
-=======
 
 export const openCircleForm = (payload) => ({
 	type: OPEN_CIRCLE_FORM,
 	payload: payload,
 })
->>>>>>> kvoli-grouppage
