39168babbbb3dab7b2039ed8d04ef040538276b0
alanlewis764 <49027863+alanlewis764@users.noreply.github.com>
2019-09-18 12:22:59 +1000

    Alan circle api (#73)
    * readd dotenv file to dependencies for server package .json
    
    * add package-lock
    
    * API endpoint to get all users from the database
    
    * api logic for circles. Need to change to get permissions working correctly.
    
    * changes to circle backend for codacy

--- client/src/js/actions/circles.js
+++ client/src/js/actions/circles.js
@@ -1,21 +1,26 @@
-import { ADD_CIRCLE, ADD_CIRCLE_USER, REMOVE_CIRCLE, REMOVE_CIRCLE_USER } from "../constants/action-types.js";
+import {
+  ADD_CIRCLE,
+  ADD_CIRCLE_USER,
+  REMOVE_CIRCLE,
+  REMOVE_CIRCLE_USER
+} from '../constants/action-types.js';
 
 export const addCircle = (payload) => ({
-	type: ADD_CIRCLE,
-	payload: payload
+  type: ADD_CIRCLE,
+  payload: payload
 });
 
 export const addCircleUser = (payload) => ({
-	type: ADD_CIRCLE_USER,
-	payload: payload
+  type: ADD_CIRCLE_USER,
+  payload: payload
 });
 
 export const removeCircle = (payload) => ({
-	type: REMOVE_CIRCLE,
-	payload: payload
+  type: REMOVE_CIRCLE,
+  payload: payload
 });
 
 export const removeCircleUser = (payload) => ({
-	type: REMOVE_CIRCLE_USER,
-	payload: payload
+  type: REMOVE_CIRCLE_USER,
+  payload: payload
 });
