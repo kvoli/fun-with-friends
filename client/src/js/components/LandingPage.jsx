5b6e63fb7a7a57edc1011953c6a169e1034ec59a
alanlewis764 <49027863+alanlewis764@users.noreply.github.com>
2019-09-20 14:26:44 +1000

    update to design for the landing page (#88)

--- client/src/js/components/LandingPage.jsx
+++ client/src/js/components/LandingPage.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import Container from '@material-ui/core/Container';
 import { Typography, List, ListItemText, ListItem, Grid, FormGroup } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { rotateIn } from 'react-animations';
+import { rotateIn, bounce, rollIn } from 'react-animations';
 import Radium, { StyleRoot } from 'radium';
-import { circleImage } from '../../SVG/SVGImages';
+import { imageLogo, downloadLogo, communityLogo, shareLogo } from '../../SVG/SVGImages';
 
 const useStyles = makeStyles(theme => ({
   container: {
@@ -17,14 +17,18 @@ const useStyles = makeStyles(theme => ({
   },
   title: {
     flexGrow: 1,
-    marginBottom: 1,
+    marginBottom: 20,
     paddingBottom: 20,
+    marginTop: 15,
+    paddingTop: 15,
     padding: theme.spacing(0.75, 2.5, 0),
   },
   subtitle: {
     flexGrow: 1,
     marginBottom: 20,
     paddingBottom: 20,
+    marginTop: 10,
+    paddingTop: 10,
     paddingRight: 10,
     padding: theme.spacing(0, 2.5, 0),
   },
@@ -36,13 +40,35 @@ const useStyles = makeStyles(theme => ({
     height: 400,
     width: 400,
     paddingRight: 10,
+    marginLeft: 200,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  subImage: {
+    flexGrow: 1,
+    height: 60,
+    width: 60,
+    marginLeft: 100,
+    paddingLeft: 10,
+    marginTop: 10,
+    paddingTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
   },
 }));
 
 const animationStyles = {
   rotateIn: {
-    animation: 'x 5s',
+    animation: '20s',
     animationName: Radium.keyframes(rotateIn, 'rotateIn'),
+  },
+  bounce: {
+    animation: '5s',
+    animationName: Radium.keyframes(bounce, 'bounce'),
+  },
+  rollIn: {
+    animation: '5s',
+    animationName: Radium.keyframes(rollIn, 'rollIn'),
   },
 };
 
@@ -51,47 +77,52 @@ const LandingPage = () => {
 
   return (
     <Container className={classes.container} maxWidth='lg' justify='space-between' style={useStyles.container}>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={12}>
-          <Typography variant='h1' className={classes.title}>
-            Fun. With. Friends.
-          </Typography>
-          <div align='right'>
+      <FormGroup>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={12} align='center'>
+            <Typography variant='h1' className={classes.title}>
+              Fun. With. Friends.
+            </Typography>
             <FormGroup row>
-              <div>
-                <StyleRoot>
-                  <img src={circleImage} alt='' style={animationStyles.rotateIn} className={classes.image} />
-                </StyleRoot>
-              </div>
-              <div>
-                <List>
-                  <ListItem>
-                    <ListItemText>
+              <StyleRoot>
+                <img src={imageLogo} alt='' style={animationStyles.rotateIn} className={classes.image} />
+              </StyleRoot>
+              <List>
+                <ListItem>
+                  <FormGroup row>
+                    <img src={downloadLogo} alt='' className={classes.subImage} />
+                    <ListItemText align='center'>
                       <Typography className={classes.subtitle} variant='h4'>
                         Upload Your Artifacts.
                       </Typography>
                     </ListItemText>
-                  </ListItem>
-                  <ListItem>
+                  </FormGroup>
+                </ListItem>
+                <ListItem>
+                  <FormGroup row>
+                    <img src={shareLogo} alt='' className={classes.subImage} />
                     <ListItemText>
                       <Typography className={classes.subtitle} variant='h4'>
                         Share With Friends.
                       </Typography>
                     </ListItemText>
-                  </ListItem>
-                  <ListItem>
+                  </FormGroup>
+                </ListItem>
+                <ListItem>
+                  <FormGroup row>
+                    <img src={communityLogo} alt='' className={classes.subImage} />
                     <ListItemText>
                       <Typography className={classes.subtitle} variant='h4'>
                         Create Your Community.
                       </Typography>
                     </ListItemText>
-                  </ListItem>
-                </List>
-              </div>
+                  </FormGroup>
+                </ListItem>
+              </List>
             </FormGroup>
-          </div>
+          </Grid>
         </Grid>
-      </Grid>
+      </FormGroup>
     </Container>
   );
 };
